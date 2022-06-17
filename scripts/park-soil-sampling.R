4db1f440567cd3baa767beec037fb1ff93f7fb63
icrichmond <icrichmond@mun.ca>
2022-06-17 11:45:33 -0400

    fixing earthworm sampling labels

--- scripts/park-soil-sampling.R
+++ scripts/park-soil-sampling.R
@@ -13,7 +13,7 @@ park_soil_sampling <- c(
   # randomly sample one point within each of the four polygons / buffer
   tar_target(
     soil_core_pts,
-    st_sample(soil_grid, size = c(1,1), type = "random", by_polygon = T) %>% 
+    st_sample(soil_grid, size = c(1,1), type = "random", by_polygon = T, exact = T) %>% 
       st_as_sf() %>% 
       mutate(PlotID = soil_grid$PlotID)
   ),
@@ -21,20 +21,22 @@ park_soil_sampling <- c(
   # randomly sample three points within each buffer for earthworm quadrats
   tar_target(
     worm_pts_geom,
-    st_sample(sp_pts_buff, size = c(3,3), type = "random", by_polygon = T) %>% st_as_sf()
-  ),
-  
-  tar_target(
-    worm_pts_int,
-    st_join(worm_pts_geom, sp_pts_buff)
+    st_sample(sp_pts_buff, size = c(3,3), type = "random", by_polygon = T) %>% 
+      st_as_sf() %>%
+      mutate(GroupID = rep(seq_len(nrow(.)/3), each=3))
   ),
   
   # create unique ids for earthworm quadrats
   tar_target(
     worm_pts,
-    worm_pts_int %>%
-      group_by(PlotID) %>%
-      mutate(QuadratID = paste0(PlotID, "-", 0, row_number()))
+    sp_pts_buff %>% 
+      ungroup() %>% 
+      mutate(GroupID = row_number()) %>% 
+      st_drop_geometry() %>% 
+      left_join(worm_pts_geom, ., by = "GroupID") %>% 
+      group_by(PlotID) %>% 
+      mutate(QuadratID = paste0(PlotID, "-", 0, row_number())) %>%
+      select(-GroupID)
     
   ),
   
