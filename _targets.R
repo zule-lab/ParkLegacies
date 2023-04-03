00adfe09cae397f5af20b5aa7cf303fd0d267cce
icrichmond <icrichmond@mun.ca>
2023-04-03 13:04:03 -0400

    update with renv and tar_source

--- _targets.R
+++ _targets.R
@@ -13,15 +13,18 @@ lapply(dir('R', '*.R', full.names = TRUE), source)
 tar_option_set(format = 'qs')
 
 
+# Renv --------------------------------------------------------------------
+activate()
+snapshot()
+restore()
+
+
 # Variables ---------------------------------------------------------------
 
 
 # Scripts -----------------------------------------------------------------
-source(file.path('scripts', 'park-prep.R'))
-source(file.path('scripts', 'park-historical-boundaries.R'))
-source(file.path('scripts', 'park-sampling.R'))
-source(file.path('scripts', 'park-soil-sampling.R'))
+tar_source('scripts')
 
 # Targets: all ------------------------------------------------------------
 # Automatically grab all the "park" lists above
-lapply(grep('park', ls(), value = TRUE), get)
+lapply(grep('target', ls(), value = TRUE), get)
