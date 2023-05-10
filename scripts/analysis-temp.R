f5b9996a9b07e946b1fdf91f9786cab39249a8af
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2023-05-10 11:53:48 -0400

    including both years in function + making site changes

--- scripts/analysis-temp.R
+++ scripts/analysis-temp.R
@@ -2,7 +2,12 @@ target_analysis_temp <- c(
   
   tar_target(
     temp_sp_pts_2022,
-    sp_pts_temp_2022(field_sp_pts)
+    sp_pts_temp(field_sp_pts, 2022)
+  ),
+  
+  tar_target(
+    temp_sp_pts_2023,
+    sp_pts_temp(field_sp_pts, 2023)
   )
   
   
