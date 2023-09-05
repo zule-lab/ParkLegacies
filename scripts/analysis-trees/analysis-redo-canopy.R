4e4abb26aba5039723088666342e9eda50531fcc
icrichmond <icrichmond@mun.ca>
2023-09-05 14:17:44 -0400

    clarify title

---
+++ scripts/analysis-trees/analysis-redo-canopy.R
@@ -0,0 +1,15 @@
+target_analysis_redo_canopy <- c(
+
+  tar_file_read(
+    cc_2021,
+    'input/canopy/660_IndiceCanopee_2021.tif',
+    read_stars(!!.x)
+  ),
+  
+  tar_target(
+    field_cc,
+    canopy_cover(sp_pts, field_sp_pts, cc_2021)
+  )
+  
+  
+)
