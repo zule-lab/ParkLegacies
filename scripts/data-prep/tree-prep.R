60363daa0932eb5d93f035d3ad92780ade73d21e
icrichmond <icrichmond@mun.ca>
2023-10-05 12:54:16 -0400

    calculate tree indices

--- scripts/data-prep/tree-prep.R
+++ scripts/data-prep/tree-prep.R
@@ -10,6 +10,11 @@ target_tree_prep <- c(
   tar_target(
     trees_clean,
     clean_trees(trees_raw, field_cc)
+  ),
+  
+  tar_target(
+    trees_indices,
+    indices_trees(trees_clean, full_study_parks)
   )
   
 
