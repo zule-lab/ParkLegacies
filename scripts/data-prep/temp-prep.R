aafc6e09f29fbff2b9a7b42d3823510fd8f57684
icrichmond <icrichmond@mun.ca>
2023-10-06 14:42:31 -0400

    read in temp data

---
+++ scripts/data-prep/temp-prep.R
@@ -0,0 +1,25 @@
+target_temp_prep <- c(
+  
+  tar_files(
+    temp_files,
+    dir('input/temperature', full.names = TRUE)
+  ),
+  
+  tar_target(
+    temp_dfs, 
+    read.csv(temp_files) %>%
+      mutate(PlotID = str_extract(basename(xfun::sans_ext(temp_files)), "[^_]+")),
+    pattern = map(temp_files)
+  ),
+  
+  tar_target(
+    temp_clean,
+    clean_temp(temp_dfs)
+  )
+  
+  
+  
+  
+  
+  
+)
