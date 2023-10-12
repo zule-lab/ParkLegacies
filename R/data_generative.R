c3a5ec91734bb25f4a007009bf6bb3b2aece23a5
icrichmond <icrichmond@mun.ca>
2023-10-12 12:23:16 -0400

    start model workflow outline

---
+++ R/data_generative.R
@@ -0,0 +1,16 @@
+data_generative <- function(){
+  
+  # response: difference in temperature 
+  
+  # explanatories
+  # past land-use
+  
+  # tree size
+  
+  # tree diversity
+  
+  # tree density
+  
+  
+  
+}
--- scripts/analysis-models/models.R
+++ scripts/analysis-models/models.R
@@ -1,5 +1,18 @@
 targets_models <- c(
   
+  tar_target(
+    generative_data,
+    data_generative()
+  )#,
   
+  #tar_target(
+  #  generative_model,
+  #  model_generative()
+  #),
+  #
+  #tar_target(
+  #  data_model,
+  #  model_data()
+  #)
   
 )
