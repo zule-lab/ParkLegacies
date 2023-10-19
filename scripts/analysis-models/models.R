6bc2af07f3c00743c4683ed5500d8821a7f39874
icrichmond <icrichmond@mun.ca>
2023-10-19 14:09:47 -0400

    model generated data

--- scripts/analysis-models/models.R
+++ scripts/analysis-models/models.R
@@ -6,8 +6,23 @@ targets_models <- c(
   ),
   
   tar_target(
-    model_data,
+    real_data,
     data_model(temp_indices)
+  ),
+  
+  tar_target(
+    gen_model,
+    brm(temp ~ group + dens_s + size_s + richness_s,
+        data = generative_data,
+        family = gaussian(),
+        priors = c(
+          prior(normal(0, 0.5), class = "Intercept"),
+          prior(normal(0, 0.5), class = "b"),
+          prior(exponential(2), class = "sigma")
+        ),
+        backend = 'cmdstanr',
+        iter = 1000,
+        chains = 8)
   )
   
 )
