5cc68daa9efed9e6606af157445febd3c47168a3
icrichmond <icrichmond@mun.ca>
2023-10-31 14:20:19 -0400

    scale data

---
+++ R/data_model.R
@@ -0,0 +1,13 @@
+data_model <- function(temp_indices){
+  
+  # model is temp ~ past land-use + tree density + tree size + tree diversity 
+  data <- temp_indices %>%
+    ungroup() %>% 
+    mutate(PastLandUse = as.factor(PastLandUse),
+           SR_L_s = scale(SR_L), 
+           DBH_med_L_s = scale(DBH_med_L), 
+           Dens_L_s = scale(Dens_L)) %>% 
+    select(c(max_day, max_night, mean_day, mean_night, PastLandUse, SR_L_s, DBH_med_L_s, Dens_L_s))
+  
+  
+}
