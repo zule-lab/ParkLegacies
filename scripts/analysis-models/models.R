5ceadb065647ed04774dc6dfca51e39b7bd4c3fa
icrichmond <icrichmond@mun.ca>
2023-10-31 13:41:14 -0400

    tighten priors, add diagnostics

--- scripts/analysis-models/models.R
+++ scripts/analysis-models/models.R
@@ -16,14 +16,25 @@ targets_models <- c(
         data = generative_data,
         family = gaussian(),
         prior = c(
-          prior(normal(0, 1), class = "Intercept"),
-          prior(normal(0, 1), class = "b"),
-          prior(exponential(2), class = "sigma")
+          prior(normal(0, 0.5), class = "Intercept"),
+          prior(normal(0, 0.5), class = "b"),
+          prior(exponential(1), class = "sigma")
         ),
         sample_prior = 'only',
         backend = 'cmdstanr',
         iter = 1000,
         chains = 8)
+  ),
+  
+  tar_target(
+    prior_diagnostics,
+    model_diagnostics(model_prior, 'prior_only')
+  ),
+  
+  tar_target(
+    prior_range,
+    conditional_effects(model_prior)
+    # prior ranges look good
   ),
   
   tar_target(
@@ -39,6 +50,11 @@ targets_models <- c(
         backend = 'cmdstanr',
         iter = 1000,
         chains = 8)
+  ),
+  
+  tar_target(
+    generative_diagnostics,
+    model_diagnostics(gen_model, 'generative')
   )
   
 )
