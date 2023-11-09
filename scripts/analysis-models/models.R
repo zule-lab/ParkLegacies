9890af3c74fc77ab973fb8d99e18e0e90dec5fd3
icrichmond <icrichmond@mun.ca>
2023-11-09 11:03:08 -0500

    change models

--- scripts/analysis-models/models.R
+++ scripts/analysis-models/models.R
@@ -6,14 +6,22 @@ targets_models <- c(
   ),
   
   tar_target(
-    real_data,
-    data_model(temp_indices)
+    real_data_cooling,
+    data_model(temp_indices, 'cooling')
   ),
   
   tar_target(
-    prior_model,
-    brm(temp ~ 1 + group:dens_s + group:size_s + group:richness_s,
-        data = generative_data,
+    real_data_landuse,
+    data_model(temp_indices, 'landuse')
+  ),
+
+# Assess priors -----------------------------------------------------------
+
+  # Model 1: direct effect of tree variables on cooling and how those effects vary by past land use type 
+  tar_target(
+    prior_model_1,
+    brm(mean_day ~ 1 +  Dens_L_s + DBH_med_L_s + SR_L_s + (1 | PastLandUse) + (1 | Park),
+        data = real_data_cooling,
         family = gaussian(),
         prior = c(
           prior(normal(0, 0.5), class = "Intercept"),
@@ -26,50 +34,93 @@ targets_models <- c(
         chains = 8)
   ),
   
+  # Model 2-4: total effect of past land use type on tree variables 
   tar_target(
-    prior_diagnostics,
-    model_diagnostics(prior_model, 'prior_only')
-  ),
-  
-  tar_target(
-    prior_range,
-    conditional_effects(prior_model)
-    # prior ranges look representative of data ranges
-  ),
-  
-  tar_target(
-    gen_model,
-    brm(temp ~ 1 + group:dens_s + group:size_s + group:richness_s,
-        data = generative_data,
+    prior_model_2,
+    brm(Dens_L ~ 1 + (1 | PastLandUse),
+        data = real_data_landuse,
         family = gaussian(),
         prior = c(
           prior(normal(0, 0.5), class = "Intercept"),
-          prior(normal(0, 0.5), class = "b"),
           prior(exponential(1), class = "sigma")
         ),
+        sample_prior = 'only',
+        backend = 'cmdstanr',
+        iter = 1000,
+        chains = 8)
+  ),
+
+  tar_target(
+    prior_model_3,
+    brm(DBH_med_L ~ 1 + (1 | PastLandUse),
+        data = real_data_landuse,
+        family = gaussian(),
+        prior = c(
+          prior(normal(0, 0.5), class = "Intercept"),
+          prior(exponential(1), class = "sigma")
+        ),
+        sample_prior = 'only',
+        backend = 'cmdstanr',
+        iter = 1000,
+        chains = 8)
+  ),
+
+  tar_target(
+    prior_model_4,
+    brm(SR_L ~ 1 + (1 | PastLandUse),
+        data = real_data_landuse,
+        family = gaussian(),
+        prior = c(
+          prior(normal(0, 0.5), class = "Intercept"),
+          prior(exponential(1), class = "sigma")
+        ),
+        sample_prior = 'only',
         backend = 'cmdstanr',
         iter = 1000,
         chains = 8)
   ),
   
+  # Model 5: total effect of past land-use type on cooling
   tar_target(
-    generative_diagnostics,
-    model_diagnostics(gen_model, 'generative')
-  ),
-  
-  tar_target(
-    max_day_model,
-    brm(max_day ~ 1 + PastLandUse:Dens_L_s +  PastLandUse:SR_L_s +  PastLandUse:DBH_med_L_s,
-        data = real_data,
+    prior_model,
+    brm(mean_day ~ 1 + (1 | PastLandUse) + (1 | Park),
+        data = real_data_cooling,
         family = gaussian(),
         prior = c(
           prior(normal(0, 0.5), class = "Intercept"),
-          prior(normal(0, 0.5), class = "b"),
           prior(exponential(1), class = "sigma")
         ),
+        sample_prior = 'only',
         backend = 'cmdstanr',
         iter = 1000,
         chains = 8)
   )
+
+# Assess models -----------------------------------------------------------
+
+  # Test model to see if it is returning what we expect w generative data 
+  #tar_target(
+  #  gen_model,
+  #  brm(temp ~ 1 + (1 + dens_s | group) + (1 + size_s | group) + (1 + richness_s | group),
+  #      data = generative_data,
+  #      family = gaussian(),
+  #      prior = c(
+  #        prior(normal(0, 0.5), class = "Intercept"),
+  #        prior(normal(0, 0.5), class = "b"),
+  #        prior(exponential(1), class = "sigma")
+  #      ),
+  #      backend = 'cmdstanr',
+  #      iter = 1000,
+  #      chains = 8)
+  #),
   
+  #tar_target(
+  #  generative_diagnostics,
+  #  model_diagnostics(gen_model, 'generative')
+  #)
+  
+
+# Assess data -------------------------------------------------------------
+
+
 )
