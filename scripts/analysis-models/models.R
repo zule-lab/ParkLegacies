6437adcf232cc35c79eac1c991aa0612905c96d7
icrichmond <icrichmond@mun.ca>
2023-11-15 09:19:50 -0500

    zar_brms + remove generative

--- scripts/analysis-models/models.R
+++ scripts/analysis-models/models.R
@@ -1,11 +1,6 @@
 targets_models <- c(
   
   tar_target(
-    generative_data,
-    data_generative()
-  ),
-  
-  tar_target(
     real_data_cooling,
     data_model(temp_indices, 'cooling')
   ),
@@ -15,112 +10,91 @@ targets_models <- c(
     data_model(temp_indices, 'landuse')
   ),
 
-# Assess priors -----------------------------------------------------------
+# Modelling -----------------------------------------------------------
 
   # Model 1: direct effect of tree variables on cooling and how those effects vary by past land use type 
-  tar_target(
-    prior_model_1,
-    brm(mean_day ~ 1 +  Dens_L_s + DBH_med_L_s + SR_L_s + (1 | PastLandUse) + (1 | Park),
-        data = real_data_cooling,
-        family = gaussian(),
-        prior = c(
-          prior(normal(0, 0.5), class = "Intercept"),
-          prior(normal(0, 0.5), class = "b"),
+  zar_brms(
+    model_1,
+    formula = mean_day ~ 1 + Dens_L_s + DBH_med_L_s + SR_L_s + (1 | Park),
+    family = gaussian(),
+    prior = c( 
+          prior(normal(0, 1), class = "b"),
+          prior(normal(0, 1), class = "Intercept"),
+          prior(exponential(1), class = "sd"),
           prior(exponential(1), class = "sigma")
         ),
-        sample_prior = 'only',
-        backend = 'cmdstanr',
-        iter = 1000,
-        chains = 8)
+    backend = 'cmdstanr',
+    data = real_data_cooling,
+    chains = 2,
+    iter = 1000,
+    cores = 2
   ),
   
   # Model 2-4: total effect of past land use type on tree variables 
-  tar_target(
-    prior_model_2,
-    brm(Dens_L ~ 1 + (1 | PastLandUse),
-        data = real_data_landuse,
-        family = gaussian(),
-        prior = c(
-          prior(normal(0, 0.5), class = "Intercept"),
-          prior(exponential(1), class = "sigma")
-        ),
-        sample_prior = 'only',
-        backend = 'cmdstanr',
-        iter = 1000,
-        chains = 8)
+  zar_brms(
+    model_2,
+    formula = Dens_L ~ 1 + (1 | PastLandUse),
+    data = real_data_landuse,
+    family = gaussian(),
+    prior = c(
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(exponential(1), class = "sd"),
+      prior(exponential(1), class = "sigma")
+    ),
+    backend = 'cmdstanr',
+    iter = 1000,
+    chains = 2,
+    cores = 2
   ),
 
-  tar_target(
-    prior_model_3,
-    brm(DBH_med_L ~ 1 + (1 | PastLandUse),
-        data = real_data_landuse,
-        family = gaussian(),
-        prior = c(
-          prior(normal(0, 0.5), class = "Intercept"),
-          prior(exponential(1), class = "sigma")
-        ),
-        sample_prior = 'only',
-        backend = 'cmdstanr',
-        iter = 1000,
-        chains = 8)
+  zar_brms(
+    model_3,
+    formula = DBH_med_L ~ 1 + (1 | PastLandUse),
+    data = real_data_landuse,
+    family = gaussian(),
+    prior = c(
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(exponential(1), class = "sd"),
+      prior(exponential(1), class = "sigma")
+    ),
+    backend = 'cmdstanr',
+    iter = 1000,
+    chains = 2,
+    cores = 2
   ),
 
-  tar_target(
-    prior_model_4,
-    brm(SR_L ~ 1 + (1 | PastLandUse),
-        data = real_data_landuse,
-        family = gaussian(),
-        prior = c(
-          prior(normal(0, 0.5), class = "Intercept"),
-          prior(exponential(1), class = "sigma")
-        ),
-        sample_prior = 'only',
-        backend = 'cmdstanr',
-        iter = 1000,
-        chains = 8)
+  zar_brms(
+    model_4,
+    formula = SR_L ~ 1 + (1 | PastLandUse),
+    data = real_data_landuse,
+    family = gaussian(),
+    prior = c(
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(exponential(1), class = "sd"),
+      prior(exponential(1), class = "sigma")
+    ),
+    backend = 'cmdstanr',
+    iter = 1000,
+    chains = 2,
+    cores = 2
   ),
   
   # Model 5: total effect of past land-use type on cooling
-  tar_target(
-    prior_model,
-    brm(mean_day ~ 1 + (1 | PastLandUse) + (1 | Park),
-        data = real_data_cooling,
-        family = gaussian(),
-        prior = c(
-          prior(normal(0, 0.5), class = "Intercept"),
-          prior(exponential(1), class = "sigma")
-        ),
-        sample_prior = 'only',
-        backend = 'cmdstanr',
-        iter = 1000,
-        chains = 8)
+  zar_brms(
+    model_5,
+    formula = mean_day ~ 1 + PastLandUse + Age + (1 | Park),
+    data = real_data_cooling,
+    family = gaussian(),
+    prior = c( 
+      prior(normal(0, 0.5), class = "b"),
+      prior(normal(0, 0.5), class = "Intercept"),
+      prior(exponential(1), class = "sd"),
+      prior(exponential(1), class = "sigma")
+    ),
+    backend = 'cmdstanr',
+    iter = 1000,
+    chains = 2,
+    cores = 2
   )
-
-# Assess models -----------------------------------------------------------
-
-  # Test model to see if it is returning what we expect w generative data 
-  #tar_target(
-  #  gen_model,
-  #  brm(temp ~ 1 + (1 + dens_s | group) + (1 + size_s | group) + (1 + richness_s | group),
-  #      data = generative_data,
-  #      family = gaussian(),
-  #      prior = c(
-  #        prior(normal(0, 0.5), class = "Intercept"),
-  #        prior(normal(0, 0.5), class = "b"),
-  #        prior(exponential(1), class = "sigma")
-  #      ),
-  #      backend = 'cmdstanr',
-  #      iter = 1000,
-  #      chains = 8)
-  #),
   
-  #tar_target(
-  #  generative_diagnostics,
-  #  model_diagnostics(gen_model, 'generative')
-  #)
-  
-
-# Assess data -------------------------------------------------------------
-
-
 )
