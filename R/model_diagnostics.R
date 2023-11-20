db6e775152dcb4ed9a47608e46efab9b03c3f9da
icrichmond <52459005+icrichmond@users.noreply.github.com>
2023-11-20 11:46:29 -0500

    add pp_check

--- R/model_diagnostics.R
+++ R/model_diagnostics.R
@@ -2,8 +2,10 @@ model_diagnostics <- function(model_fit, model_name){
   
   pdf(paste0('graphics/diagnostics/', model_name, '.pdf'))
   
+  # basic model fit plots
   plot(model_fit)
   
+  # Gelman plot
   ac <- as_draws_df(model_fit) %>% 
     mutate(chain = .chain) %>% 
     mcmc_acf(pars = vars(b_Intercept), lags = 35)
@@ -11,6 +13,9 @@ model_diagnostics <- function(model_fit, model_name){
   plot(ac)
   
   coda::gelman.plot(as.mcmc(model_fit)[, "b_Intercept", ])
+  
+  # posterior check
+  brms::pp_check(model_fit, ndraws = 100)
   
   dev.off()
 
