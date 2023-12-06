077e8a0dd8e9b483ab56d8c99a146e32cbc508e4
icrichmond <52459005+icrichmond@users.noreply.github.com>
2023-12-06 16:17:46 -0500

    conflictssssss

--- R/packages.R
+++ R/packages.R
@@ -1,44 +1,44 @@
 # === Packages ------------------------------------------------------------
 # Framework by Alec L. Robitaille
 
-library(conflicted)
-
-library(targets)
-library(tarchetypes)
-library(renv)
-
-library(data.table)
-library(dplyr)
-library(tidyr)
-library(purrr)
-library(stringr)
-library(readr)
-library(tibble)
-
-library(ggplot2)
-library(ggdag)
-library(ggspatial)
-library(ggdist)
-library(grid)
-library(patchwork)
-library(gridExtra)
-
-library(datetime)
-
-library(iNEXT)
-library(zarg)
-
-library(suncalc)
-
-library(xfun)
-library(janitor)
-
-library(knitr)
-library(rmarkdown)
-
-library(downloader)
-
 suppressPackageStartupMessages({
+  library(conflicted)
+  
+  library(targets)
+  library(tarchetypes)
+  library(renv)
+  
+  library(data.table)
+  library(dplyr)
+  library(tidyr)
+  library(purrr)
+  library(stringr)
+  library(readr)
+  library(tibble)
+  
+  library(ggplot2)
+  library(ggdag)
+  library(ggspatial)
+  library(ggdist)
+  library(grid)
+  library(patchwork)
+  library(gridExtra)
+  
+  library(datetime)
+  
+  library(iNEXT)
+  library(zarg)
+  
+  library(suncalc)
+  
+  library(xfun)
+  library(janitor)
+  
+  library(knitr)
+  library(rmarkdown)
+  
+  library(downloader)
+  
   library(sf)
   library(stars)
   library(terra)
@@ -51,14 +51,56 @@ suppressPackageStartupMessages({
   library(cmdstanr)
   library(tidybayes)
   library(bayesplot)
+  
 })
 
 conflicts_prefer(renv::autoload, 
                  renv::embed, 
                  renv::history, 
+                 renv::modify,
+                 renv::update,
+                 renv::upgrade,
+                 renv::init,
                  base::`:`,
                  base::`%in%`,
+                 base::load,
+                 base::remove,
+                 base::is.double,
+                 base::xor,
+                 base::order,
+                 base::rank,
+                 utils::hashtab,
+                 data.table::transpose,
+                 data.table::setattr,
                  dplyr::filter,
                  dplyr::first,
+                 dplyr::between,
+                 dplyr::combine,
+                 dplyr::lag,
+                 dplyr::last,
+                 dplyr::setdiff,
+                 dplyr::setequal,
+                 dplyr::union,
+                 dplyr::symdiff,
                  base::match,
-                 .quiet = T) 
+                 brms::ar,
+                 brms::autocor,
+                 brms::dstudent_t,
+                 brms::pstudent_t,
+                 brms::qstudent_t,
+                 brms::rstudent_t,
+                 bayesplot::rhat,
+                 terra::spin,
+                 terra::crosstab,
+                 terra::area,
+                 terra::depth,
+                 terra::intersect,
+                 terra::extract,
+                 terra::shift,
+                 terra::project,
+                 janitor::chisq.test,
+                 janitor::fisher.test,
+                 rmarkdown::run,
+                 xfun::attr,
+                 xfun::isFALSE,
+                 .quiet = T) #
