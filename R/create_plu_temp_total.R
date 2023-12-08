4c14e024609ec93f742d7c195a70e44897f1560e
icrichmond <52459005+icrichmond@users.noreply.github.com>
2023-12-08 15:48:48 -0500

    total effect of plu

---
+++ R/create_plu_temp_total.R
@@ -0,0 +1,24 @@
+create_plu_temp_total <- function(model_list, temp_indices){
+
+  model_5 <- model_list[[7]]
+  
+  t <- model_5 %>%
+    epred_draws(expand_grid(PastLandUse = c('Agricultural', 'Forested', 'Industrial'),
+                            Age_s = 0,
+                            tod = c('day', 'night')),
+                re_formula = ~ (1 | tod)) %>%
+    ggplot(aes(x = .epred, fill = PastLandUse, color = tod)) +
+    stat_halfeye() +
+    scale_color_viridis_d(direction = -1) + 
+    scale_fill_manual(values = c( "#c2d6a4", "#669d62","#1e3d14"), breaks = c('Industrial', 'Agricultural', 'Forested')) + 
+    theme_classic() + 
+    theme(strip.background = element_blank(),
+          strip.text.y = element_blank()) +
+    labs(x = "Cooling Effect - Mean Cooling Effect", y = "",
+         color = "", 
+         fill = "") + 
+    facet_grid(rows = vars(factor(PastLandUse, levels=c('Industrial', 'Agricultural', 'Forested'))))
+  
+  ggsave('graphics/plu_temp_total.png', t, width = 12, height = 10, units = 'in')
+  
+}
