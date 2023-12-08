0cdc0bb34ab77396807bccd8ba3bfe586af8cf35
icrichmond <52459005+icrichmond@users.noreply.github.com>
2023-12-08 10:01:59 -0500

    change values to island of mtl instead of city

--- R/data_socioec.R
+++ R/data_socioec.R
@@ -2,12 +2,12 @@ data_socioec <- function(census){
   
   # montreal mean income extracted from the 2021 Census Profile
   # https://www12.statcan.gc.ca/census-recensement/2021/dp-pd/prof/details/page.cfm?Lang=E&SearchText=Montreal&DGUIDlist=2021A00052466023&GENDERlist=1,2,3&STATISTIClist=1,4&HEADERlist=0
-  mtlpopdens <- 4833.5
-  mtlincome <- 36800
-  mtlvismin <- 0.388
-  mtlrecimm <- 0.06 	
-  mtledu <- 0.361
-  mtlside <- 0.071
+  mtlpopdens <- 4022.3
+  mtlincome <- 37600
+  mtlvismin <- 0.376
+  mtlrecimm <- 0.058 	
+  mtledu <- 0.370
+  mtlside <- 0.111
   
   df <- census %>% 
     group_by(PastLandUse) %>% 
