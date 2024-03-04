56f9d4ffe0dff54684731e3ddb1e067c17073d0e
icrichmond <bellarichmond@proton.me>
2024-03-04 14:28:24 -0500

    change destinations

--- scripts/data-prep/park-prep.R
+++ scripts/data-prep/park-prep.R
@@ -11,14 +11,14 @@ target_park_prep <- c(
   tar_target(
     city_parks, 
     download_shp("https://data.montreal.ca/dataset/2e9e4d2f-173a-4c3d-a5e3-565d79baa27d/resource/c57baaf4-0fa8-4aa4-9358-61eb7457b650/download/shapefile.zip",
-                 "input/park_boundaries/mon_park_raw.zip")
+                 "input/mon_park_raw/")
     ),
   
   # download shapefiles of historical industrial sites
   tar_target(
     ind_bound, 
     download_shp("https://data.montreal.ca/dataset/d332b1ef-95af-42d8-a996-7e451f1c6722/resource/4197dcf3-0f90-4034-b021-85c52ba4c9c2/download/anciennes_carrieres_depot_surface.zip",
-                 "input/park_boundaries/mon_historical_ind_boundaries.zip")
+                 "input/mon_historical_ind_boundaries/")
   ),
   
   # hand-drawn boundaries of agricultural boundaries in parks
