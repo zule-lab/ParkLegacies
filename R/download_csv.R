90bfc1ad47ace02a5b9d6414b10205ea57980665
icrichmond <bellarichmond@proton.me>
2024-03-04 14:27:31 -0500

    use temp file

--- R/download_csv.R
+++ R/download_csv.R
@@ -1,12 +1,12 @@
-download_csv <- function(url, dest){
+download_csv <- function(url){
   
   temp <- tempfile()
     
-  download.file(url, dest, mode = "wb")
+  download.file(url, temp, mode = "wb")
   
-  master <- as.character(unzip(dest, list = TRUE)$Name)
+  master <- as.character(unzip(temp, list = TRUE)$Name)
   
-  df <- read.csv(unz("input/quebec_census_2021/census.zip", master[1]))
+  df <- read.csv(unz(temp, master[1]))
 
   return(df)
 }
