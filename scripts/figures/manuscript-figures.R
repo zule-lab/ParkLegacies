3e1a2b80e84e917790882547c513ceacc09c6b30
icrichmond <bellarichmond@proton.me>
2025-01-16 11:21:38 -0500

    use field pts for fig 2

--- scripts/figures/manuscript-figures.R
+++ scripts/figures/manuscript-figures.R
@@ -7,7 +7,7 @@ target_manuscript_figures <- c(
   
   tar_target(
     figure_2, 
-    create_figure_2(full_study_parks, sp_pts)
+    create_figure_2(full_study_parks, field_sp_pts)
   ),
   
   tar_target(
