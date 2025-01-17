2b3d29153f178e87c079621750bd89b1acdc4e26
icrichmond <bellarichmond@proton.me>
2025-01-17 14:36:36 -0500

    compute contrasts

--- R/create_plu_tree_direct.R
+++ R/create_plu_tree_direct.R
@@ -1,14 +1,14 @@
-create_plu_tree_direct <- function(model_list){
+create_plu_tree_direct <- function(model_list, temp_indices){
   
   tree_mods <- model_list[c('model_2_L', 'model_2_S', 'model_3_L', 'model_3_S', 'model_4_L')]
   
-  names(tree_mods) <- c('Mean Density (trees / square meter)', 'Mean Density (trees / square meter)', 'Median Size (DBH cm)', 'Median Tree Size (DBH cm)', 'Mean Species Richness (# species)')
+  names(tree_mods) <- c('Mean Basal Area (square meter / ha)', 'Mean Basal Area (square meter / ha)', 'Median Tree Size (DBH cm)', 'Median Tree Size (DBH cm)', 'Mean Species Richness (# species)')
   
-  mod2l <- make_plot(tree_mods[[1]], names(tree_mods[1])) + ggtitle('Large Trees (>= 5 cm DBH)')
-  mod2s <- make_plot(tree_mods[[2]], names(tree_mods[2])) + ggtitle('Small Trees (< 5 cm DBH)') 
-  mod3l <- make_plot(tree_mods[[3]], names(tree_mods[3]))
-  mod3s <- make_plot(tree_mods[[4]], names(tree_mods[4]))
-  mod4l <- make_plot(tree_mods[[5]], names(tree_mods[5]))
+  mod2l <- make_plot(tree_mods[[1]], temp_indices, names(tree_mods[1])) + ggtitle('Large Trees (>= 5 cm DBH)')
+  mod2s <- make_plot(tree_mods[[2]], temp_indices, names(tree_mods[2])) + ggtitle('Small Trees (< 5 cm DBH)') 
+  mod3l <- make_plot(tree_mods[[3]], temp_indices, names(tree_mods[3]))
+  mod3s <- make_plot(tree_mods[[4]], temp_indices, names(tree_mods[4]))
+  mod4l <- make_plot(tree_mods[[5]], temp_indices, names(tree_mods[5]))
   
   
   p <- mod2l + mod3l + mod4l + mod2s + mod3s + guide_area() + plot_layout(guides = "collect")
@@ -20,20 +20,35 @@ create_plu_tree_direct <- function(model_list){
 
 
 
-make_plot <- function(model, label){
+make_plot <- function(model, df, label){
   
-  model %>%
-    spread_draws(`b_Intercept`, r_PastLandUse[condition,]) %>%
-    mutate(condition_mean = exp(b_Intercept + r_PastLandUse),
-           condition = factor(condition, levels=c("Forested", "Agricultural", "Industrial"))) %>%
-    ggplot(aes(y = condition, x = condition_mean, fill = condition)) +
-    scale_fill_manual(values = c( "#1e3d14", "#669d62", "#c2d6a4")) +  
-    stat_halfeye() + 
-    theme_classic() + 
-    theme(legend.position = 'none',
-          plot.title = element_text(hjust = 0.5, size = 14, face = 'bold',
+  response <- colnames(model$data)[1] %>% str_remove("_s")
+  
+  mod_draws <- model %>%
+    emmeans(~ PastLandUse,
+            at = list(PastLandUse = c("Industrial", "Agricultural", "Forested")),
+            epred = TRUE, re_formula = NA) %>%
+    contrast(method = "pairwise") %>%
+    gather_emmeans_draws() 
+  
+  s <- ggplot(mod_draws, aes(x = .value, fill = contrast)) +
+    stat_halfeye(alpha = 0.7) +
+    scale_fill_manual(values = c( "#1e3d14", "#669d62", "#c2d6a4")) + 
+    labs(y = "Density", fill = "") +
+    theme_classic() +
+    theme(plot.title = element_text(hjust = 0.5, size = 14, face = 'bold',
                                     color = "black")) +
-    labs(y = "", x = label)  
+    labs(x = label)
+  
+  # get axis breaks
+  satx <- c(as.numeric(na.omit(layer_scales(s)$x$break_positions())))
+  
+  # unscale x axis
+  s +
+    scale_x_continuous(name = label,
+                       breaks = satx,
+                       labels = (round(satx * (sd(pull(df, response), na.rm = T)), 1)))
+  
     
     
 }
